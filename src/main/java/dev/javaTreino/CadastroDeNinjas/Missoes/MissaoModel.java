3070c477e8dc2c485498921f67d7694b428f5b10
unknown <andreluissc1214@gmail.com>
2025-09-11 15:06:53 -0300

    organizando pastas e criando o package e o model de missoes

---
+++ src/main/java/dev/javaTreino/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -0,0 +1,41 @@
+package dev.javaTreino.CadastroDeNinjas.Missoes;
+
+import dev.javaTreino.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_missao")
+public class MissaoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(length = 10, nullable = false)
+    private int dificuldade;
+    private NinjaModel ninjas;
+
+    public MissaoModel() {
+    }
+
+    public MissaoModel(String nome, int dificuldade) {
+        this.nome = nome;
+        this.dificuldade = dificuldade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(int dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+}
