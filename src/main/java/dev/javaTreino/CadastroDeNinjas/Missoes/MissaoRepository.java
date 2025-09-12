442f8e0df6f5d9f8e00254203c1c905020af60ff
unknown <andreluissc1214@gmail.com>
2025-09-12 14:26:31 -0300

    fix: Adiciona Column nas classes de Model

---
+++ src/main/java/dev/javaTreino/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -0,0 +1,7 @@
+package dev.javaTreino.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+
+}
--- src/main/java/dev/javaTreino/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/javaTreino/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -14,13 +14,16 @@ import lombok.NoArgsConstructor;
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name="nome")
     private String nome;
 
-    @Column(unique = true)
+    @Column(unique = true, name = "email")
     private String email;
 
+    @Column(name="idade")
     private int idade;
 
     // o ninja tem uma unica missao
