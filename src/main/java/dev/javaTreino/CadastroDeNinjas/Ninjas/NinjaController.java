cbb17b44ba977b0cefaa9fde5455a2e4346c33f8
unknown <andreluissc1214@gmail.com>
2025-09-24 17:36:42 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/javaTreino/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/javaTreino/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,7 @@
 package dev.javaTreino.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.sql.Update;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +10,31 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String BoasVindas() {
         return "Olá, Boas Vindas";
+    }
+
+    @PostMapping("/criar_ninja")
+    public String criarNinja() {
+        return "Ninja Criada com sucesso";
+    }
+
+    @GetMapping("/todos_ninjas")
+    public String todosNinjas() {
+        return "Todos os ninjas";
+    }
+
+    @GetMapping("/ninja/{id}")
+    public String ninja(@PathVariable String id) {
+        return "Ninja " + id;
+    }
+
+    @PatchMapping("/ninja/{id}")
+    public String ninjaUpdate(@PathVariable String id) {
+        return "editando Ninja " + id;
+    }
+
+    @DeleteMapping("/ninja/{id}")
+    public String ninjaDelete(@PathVariable String id) {
+        return "deletando Ninja " + id;
     }
 
 }
