6fed56b7620b48c83236edb16a8ca4bf3d6e9d0d
unknown <andreluissc1214@gmail.com>
2025-09-29 10:01:56 -0300

    feat: Implementa DTO e MAPPER ao NINJA

--- src/main/java/dev/javaTreino/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/javaTreino/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.javaTreino.CadastroDeNinjas.Ninjas;
 
+import dev.javaTreino.CadastroDeNinjas.Missoes.MissaoModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private MissaoModel missoes;
+    private String rank;
+
 }
